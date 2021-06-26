6d9fec66a94a5341576560e02c5aef05523bcac6
Josemar Luedke <josemarluedke@gmail.com>
2021-06-25 21:04:52 -0700

    Start adding docfy header

---
+++ addon/components/docfy-header.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+interface DocfyHeaderArgs {}
+
+export default class DocfyHeader extends Component<DocfyHeaderArgs> {}
---
+++ app/components/docfy-header.js
@@ -0,0 +1,1 @@
+export { default } from 'docfy-theme-ember/components/docfy-header';
